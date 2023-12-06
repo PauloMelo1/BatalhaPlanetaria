bdbf32d5d4618cc4436d9e0d0cd439cdad549264
PauloMelo1 <115944002+PauloMelo1@users.noreply.github.com>
2023-12-06 15:00:15 -0300

    Create actions.js

---
+++ actions.js
@@ -0,0 +1,11 @@
+function showActions (playerOne, playerTwo) {
+    const chooseAction = prompt 
+    ('Escolha uma ação [1]: Aumentar Base, [2]: Atacar')
+    if (chooseAction === 1) {
+
+    }
+}
+
+function baseConstruction () {
+
+}
