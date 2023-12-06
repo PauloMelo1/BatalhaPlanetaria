3dbc293785c2148cf43b2b6fe02aeae8d9a0356a
PauloMelo1 <115944002+PauloMelo1@users.noreply.github.com>
2023-12-06 18:34:22 -0300

    Update players.js

--- players.js
+++ players.js
@@ -1,9 +1,17 @@
+class Bases {
+    construtor (cordX, cordY) {
+        this.cordX = cordX;
+        this.cordY = cordY;
+    }
+}
+
 class Players {
     constructor(name, basesCount) {
       this.name = name;
+    this.bases = new Bases(cordX, cordY);
       this.basesCount = basesCount;
     }
 }
 
-const playerOne = new Players("Paulo", 5);
+const playerOne = new Players("Paulo", 5, 4, 5);
 const playerTwo = new Players("Eduardo", 5);
