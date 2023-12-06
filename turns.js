1bbc159c859fdd2810381df2dc6f07558c12038d
PauloMelo1 <115944002+PauloMelo1@users.noreply.github.com>
2023-12-06 15:00:53 -0300

    Create turns.js

---
+++ turns.js
@@ -0,0 +1,32 @@
+const dice = require('./diceRoller');
+let turnCount = dice
+
+function turnCheck (){
+if (turnCount % 2 === 1) {
+    playerTurn = "playerOne"
+} else {playerTurn = "playerTwo"} 
+turnCheck ++}
+
+
+/*
+metodo #2
+playturn = (turnCount % 2) === 1
+
+metodo #3
+let turnCount = 4
+
+function turnCheck (){
+let playerTyrn = "play1"
+if (turnCount % 2 !== 1) {
+    playerTurn = "play2"
+}
+turnCount++
+console.log(playerTurn)}
+turnCheck()
+
+
+metodo #4
+const playTurn = turncoun % 2 === 1 ? play1 : play2
+
+
+*/
