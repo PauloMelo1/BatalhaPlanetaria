7aba9277f230ab797875f8aceac15f53a78af3c8
PauloMelo1 <115944002+PauloMelo1@users.noreply.github.com>
2024-01-05 17:01:19 -0300

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,5 +1,6 @@
 function showActions (playerOne, playerTwo) {
     const chooseAction = prompt 
+        if (turnCount < 3) {('Digite a coordenada inicial onde deseja criar a primeira base 1x1 (ex: A7)')} else {}
     ('Escolha uma ação [1]: Aumentar Base, [2]: Atacar')
     if (chooseAction === 1) {
 
