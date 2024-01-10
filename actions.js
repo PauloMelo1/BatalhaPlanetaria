f92d38abda58c36c655b1e1bff1b65a78047899e
PauloMelo1 <115944002+PauloMelo1@users.noreply.github.com>
2024-01-10 17:52:59 -0300

    Update actions.js

--- actions.js
+++ actions.js
@@ -1,16 +1,49 @@
-function showActions (playerOne, playerTwo) {
-    const chooseAction = prompt 
-        if (turnCount < 3) {('Digite a coordenada inicial onde deseja criar a primeira base 1x1 (ex: A7)')} else {}
-    ('Escolha uma ação [1]: Aumentar Base, [2]: Atacar')
-    if (chooseAction === 1) {
+// class MilitaryBasePlayerOne {
+//   constructor(name, size, positionX, positionY, health, attack, cd) {
+    // this.name = name;
+    // this.size = size;
+    // this.positionX = positionX;
+    // this.positionY = positionY;
+    // this.health = size * 10;
+    // this.attack = size;
+    // this.cd = size;
+//   }
 
-    }
+// }
+
+// const militaryBasePlayerOne = new MilitaryBasePlayerOne("base1", 3, 3, 5)
+
+// console.log(militaryBasePlayerOne)
+
+const militaryBasesP1 = []
+const civilBasesP1 = []
+
+function showAction() {
+  const chooseBaseCreation = prompt('Escolha sua ação [1]: Criar base militar, [2]: Criar assentamento civil ')
+  if (Number(chooseBaseCreation) === 1) {
+    const chooseMiliSize = prompt('Escolhe o tamanho da sua base entre 1, 3 e 5')
+    if (chooseMiliSize === 1) {
+    const input = {}
+        input.cordX = prompt('Escolha a coordenada X entre 1 e 100')
+        if (input.cordX >= 1 && input.cordX <= 101) {
+            input.cordY = prompt('Escolha a coordenada Y entre 1 e 100')
+            if (input.cordY >= 1 && input.cordY <= 100) {
+                input.name = "base" + (militaryBasesP1.length + 1)
+                input.health = 10
+                input.attack = 1
+                input.cd = 1
+                a.push(input);
+            }
+        }
+            
+        }
+    //criar e chamar funçao para coletar as coordenadas X e Y militar
+  }
+  if (Number(chooseAction) === 2) {
+    //criar e chamar funçao para coletar as coordenadas X e Y civil
+  }
 }
 
-function baseConstruction () {
-
-}
-
-function attack () {
-
+function baseP1size (size) {
+  prompt ('Escolha o tamanho da sua base entre 1, 3 e 5')
 }
