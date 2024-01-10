a78f53be5d5686ea75407c1fc724ec81945dcfd5
PauloMelo1 <115944002+PauloMelo1@users.noreply.github.com>
2024-01-10 10:51:10 -0300

    Update game.js

--- game.js
+++ game.js
@@ -5,10 +5,14 @@ importar açoes
 let inGame = true;
 
 while (inGame === true) {
-actions.showActions(playerOne, playerTwo);
-if (basesCount < 0) {
-    console.log(`Fim de jogo, o jogador ${playerWinner} venceu`)
-inGame = false)
-}
+  actions.showActions(playerOne, playerTwo);
+  if (militaryBasesPlayerTwoCount < 1) {
+    console.log(`Fim de jogo, o jogador 1 ${playerOne.name} venceu`)
+    inGame = false
+  }
+  if (militaryBasesPlayerOneCount < 1) {
+    console.log(`Fim de jogo, o jogador 2 ${playerTwo.name} venceu`)
+    inGame = false
+  }
 }
     
