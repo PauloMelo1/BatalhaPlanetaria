d1487601b26d69d0e704454fbabcdf9b8ff94e3d
PauloMelo1 <115944002+PauloMelo1@users.noreply.github.com>
2024-01-25 15:24:18 -0300

    Update game.js

--- game.js
+++ game.js
@@ -5,13 +5,20 @@ importar açoes
 let inGame = true;
 
 while (inGame === true) {
-  if (baseCreationP2.militaryBasesP2 < 1) {
-    console.log(`Fim de jogo, o jogador 1 venceu`)
+    if (turn.playerTurn = playerOne) {
+        P1actions.showAction()
+    }
+    if (turn.playerTurn = playerTwo) {
+        P2actions.showAction()
+    }
+  if (baseCreationP2.militaryBasesP2 < 1 || baseCreationP1.militaryBasesP1 < 1) {
+    if (baseCreationP2.militaryBasesP2 < 1) {
+        console.log('Fim de jogo, o jogador 1 venceu')
+    }
+    if (baseCreationP1.militaryBasesP1 < 1) {
+        console.log('Fim de jogo, o jogador 2 venceu')
+    }
     inGame = false
-  }
-  if (militaryBasesP1 < 1) {
-    console.log(`Fim de jogo, o jogador 2  venceu`)
-    inGame = false
-  }
+  } 
 }
     
